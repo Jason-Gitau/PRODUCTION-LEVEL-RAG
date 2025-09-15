71e0679829899c2b069fde675571b391d2d7254e
Jason Gitau <92223939+Jason-Gitau@users.noreply.github.com>
2025-09-15 11:11:42 +0300

    Create Setup and Installation.py

---
+++ document loading/llamaindex technical implementation of data/Setup and Installation.py
@@ -0,0 +1,11 @@
+# Install required packages
+!pip install llama-index
+!pip install llama-index-readers-file
+!pip install llama-index-readers-web
+!pip install llama-index-readers-database
+!pip install llama-index-readers-s3
+!pip install beautifulsoup4
+!pip install sqlalchemy
+!pip install boto3
+!pip install google-cloud-storage
+!pip install azure-storage-blob
